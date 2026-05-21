e81da5421971559f1cdda7ee5a71a81b9913c8f0
gcbenito1-blip <gc.benito1997@gmail.com>
2026-05-21 12:53:37 +0800

    update

--- elective_tab/eTab0.py
+++ elective_tab/eTab0.py
@@ -58,8 +58,8 @@ STOPWORDS = {
     "me","you","app","apps","egov","gov","government","its","or","do","if","can",
     "has","by","use","used","get","got","just","would","could","should","very",
     "all","from","will","your","our","had","been","were","what","when","how",
-    "one","out","no","up","im","ive","dont","cant","wont","didnt","doesnt", "ang",
-    "ng", "mga", "yung", "para", "pag", "lang", "may", "nga",
+    "one","out","no","up","im","ive","dont","cant","wont","didnt","doesnt", "ang", "nila",
+    "ng", "mga", "yung", "para", "pag", "lang", "may", "nga", "ako", "nyo", "lahat", "sana",
     "wala", "walang", "mag", "nag", "kaya", "now", "even", "pero", "kasi", "parin"
 }
 
